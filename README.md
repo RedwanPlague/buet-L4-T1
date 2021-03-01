20bf76f98ce06aaf7653d41c859dc1c631a40055
RedwanPlague <mdredwan.rh@gmail.com>
2021-03-01 13:07:55 +0600

    first commit, got AES assignment in Security Lab

---
+++ README.md
@@ -0,0 +1,2 @@
+# buet-L4-T1
+my codes and other materials of Level-4 Term-1 of BUET.
---
+++ security-lab/Assignment-1-AES/code/main.py
@@ -0,0 +1,5 @@
+from BitVector import BitVector
+
+if __name__ == '__main__':
+    bv = BitVector(intVal=1605004)
+    print(bv)
