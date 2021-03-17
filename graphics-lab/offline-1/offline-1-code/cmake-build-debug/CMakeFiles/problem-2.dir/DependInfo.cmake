d7807626473c94f66ccce16cf5461b9fbf5d7a2a
RedwanPlague <mdredwan.rh@gmail.com>
2021-03-17 16:33:11 +0600

    changed pause logic, to handle pause even before creation.

--- graphics-lab/offline-1/offline-1-code/cmake-build-debug/CMakeFiles/problem-2.dir/DependInfo.cmake
+++ graphics-lab/offline-1/offline-1-code/cmake-build-debug/CMakeFiles/problem-2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/redwan/Documents/L4-T1/all-codes/graphics-lab/offline-1-code/problem-2.cpp" "/home/redwan/Documents/L4-T1/all-codes/graphics-lab/offline-1-code/cmake-build-debug/CMakeFiles/problem-2.dir/problem-2.cpp.o"
+  "/home/redwan/Documents/L4-T1/all-codes/graphics-lab/offline-1/offline-1-code/problem-2.cpp" "/home/redwan/Documents/L4-T1/all-codes/graphics-lab/offline-1/offline-1-code/cmake-build-debug/CMakeFiles/problem-2.dir/problem-2.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
