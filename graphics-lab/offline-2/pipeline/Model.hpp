5f5ec50b89f1a392161cff4d9c89a64dc944dc48
RedwanPlague <mdredwan.rh@gmail.com>
2021-04-14 16:06:22 +0600

    changed the folder structure a bit, put 'test-cases' into 'pipeline' directory

---
+++ graphics-lab/offline-2/pipeline/Model.hpp
@@ -0,0 +1,14 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "Matrix.hpp"
+#include "Vector.hpp"
+#include <vector>
+
+struct Triangle {
+    Point a, b, c;
+};
+
+class Model {};
+
+#endif // MODEL_H
--- graphics-lab/offline-2/pipeline/main.cpp
+++ graphics-lab/offline-2/pipeline/main.cpp
@@ -1,22 +1,22 @@
 #include "Matrix.hpp"
 #include "Vector.hpp"
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+ifstream fin;
+ofstream fout;
+
+struct View {
+    Point eye;
+    Vector look, up;
+    double fovY, aspectRatio, nearDist, farDist;
+} view;
+
 int main() {
-    cout << Matrix::I << endl;
-    Vector a(0, 0, 0);
-    cout << a << endl;
-    Matrix t = Matrix::translator({0.1, 0.2, 0});
-    Matrix it = Matrix::translator({-0.1, -0.2, 0});
-    /* cout << t << endl; */
-    /* cout << t * a << endl; */
-    Matrix s = Matrix::scalar({1, 2, 1});
-    Matrix is = Matrix::scalar({2, 0.5, 1});
-    /* cout << t * s << endl; */
-    /* cout << t * s * a << endl; */
-    cout << is * it * s * t * a << endl;
-    /* cout << t * s * a << endl; */
+    fin.open("test-cases/1/scene.txt");
+
     return 0;
 }
